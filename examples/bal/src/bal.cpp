e418031f08a70314964313463b30a9dbdbad9b02
datta <datta.ramadasan@univ-bpclermont.fr>
2015-07-13 16:25:06 +0200

    little improvement on bal example

--- examples/bal/src/bal.cpp
+++ examples/bal/src/bal.cpp
@@ -13,39 +13,71 @@
 #include <boost/lexical_cast.hpp>
 #include <boost/math/special_functions/sinc.hpp>
 
-//#define USE_TOON
+#define USE_TOON
 #include <libv/lma/lma.hpp>
 #include <libv/geometry/rotation.hpp>
 
-//#include <unsupported/Eigen/MatrixFunctions>
+void apply_small_rotation_(Eigen::Matrix3d& rotation, double h, int i, int j)
+{
+  const Eigen::Vector3d col_l = rotation.col(j) - rotation.col(i) * h;
+  rotation.col(i) += rotation.col(j) * h;
+  rotation.col(j) = col_l;
+}
 
+void apply_small_rotation_x(Eigen::Matrix3d& rotation, double h) { apply_small_rotation_(rotation,h,1,2); }
+void apply_small_rotation_y(Eigen::Matrix3d& rotation, double h) { apply_small_rotation_(rotation,-h,0,2); }
+void apply_small_rotation_z(Eigen::Matrix3d& rotation, double h) { apply_small_rotation_(rotation,h,0,1); }
+
+// Pose camera = Rotation + Translation + intrinsics
 struct Camera
 {
-  Eigen::Matrix3d rotation;
-  Eigen::Vector3d translation;
-  double a,b,c;
-  Camera():rotation(Eigen::Matrix3d::Identity()),translation(0,0,0),a(0),b(0),c(0){}
-
-  void apply_rotation(const double delta[9])
-  {
-    // update rotation using exponential map
-    Eigen::Matrix3d r;
-    r << 0,-delta[2], delta[1],delta[2],0,-delta[0],-delta[1],delta[0],0;
-
-	rotation *= v::rotation_exp(r);
-    
-	//rotation *= r.exp();
-
-    // update translation
-    translation.x() += delta[3];
-    translation.y() += delta[4];
-    translation.z() += delta[5];
-    // update instrinsics
-    a += delta[6];
-    b += delta[7];
-    c += delta[8];
-  }
+  Eigen::Matrix3d rotation = Eigen::Matrix3d::Identity();
+  Eigen::Vector3d translation = {0,0,0};
+  double a=0,b=0,c=0;
 };
+
+// Point 3D = Eigen::Vector3d
+
+
+namespace lma
+{
+	// Update policy of a Camera according to 9 degres of freedom
+  // The Adl parameter enable the usage of a function defined after its use
+	void apply_increment(Camera& camera, const double delta[9], const Adl&)
+	{
+    // update rotation using exponential map : camera.rotation *= exp(skew(delta{0,1,2}))
+    v::apply_rotation(camera.rotation,{delta[0],delta[1],delta[2]});
+
+    // update translation : camera.translation += delta{3,4,5}
+    camera.translation += Eigen::Map<const Eigen::Vector3d>(delta + 3);
+
+    // update instrinsics : (camera{a,b,c} += delta{6,7,8})
+    Eigen::Map<Eigen::Array3d>(&camera.a) += Eigen::Map<const Eigen::Array3d>(delta+6);
+	}
+
+	// Only for numerical derivative:
+	// Update policy of a Camera according to the Ie parameter (h ~ 1e-8).
+  // The Adl parameter enable the usage of a function defined after its use
+	template<int I> void apply_small_increment(Camera& camera, double h, v::numeric_tag<I>, const Adl&)
+	{
+		if      (I == 0) apply_small_rotation_x(camera.rotation,h);
+    else if (I == 1) apply_small_rotation_y(camera.rotation,h);
+    else if (I == 2) apply_small_rotation_z(camera.rotation,h);
+		else if (I == 3) camera.translation.x() += h;
+		else if (I == 4) camera.translation.y() += h;
+		else if (I == 5) camera.translation.z() += h;
+		else if (I == 6) camera.a += h;
+		else if (I == 7) camera.b += h;
+		else if (I == 8) camera.c += h;
+  }
+
+  //degree of freedom of a Camera
+  template<> struct Size<Camera> { enum {value = 9}; };
+
+  // Nothing to specify for the 3D points (Eigen::Vector3d) :
+  //   - degree of freedom of an Eigen::Vector is the size.
+  //   - The update policy of an Eigen::Vector is operator+.
+}
 
 
 struct BALProblem 
@@ -55,116 +87,66 @@ struct BALProblem
     std::ifstream file(filename);
     if (!file.is_open()) return false;
 
-    file >> num_cameras_ >> num_points_ >> num_observations_;
+    size_t num_cameras,num_points,num_observations;
 
-    std::cout << boost::format("Cameras %1%, Points 3D %2%, Observations %3%")%num_cameras_%num_points_%num_observations_ << std::endl;
+    file >> num_cameras >> num_points >> num_observations;
 
-    point_index_.resize(num_observations_);
-    camera_index_.resize(num_observations_);
-    observations_.resize(num_observations_);
+    std::cout << color.yellow() << 
+      boost::format("Cameras[%1%], Points 3D[%2%], Observations[%3%]")%num_cameras%num_points%num_observations 
+      << color.reset() << std::endl;
+
+    point_index.resize(num_observations);
+    camera_index.resize(num_observations);
+    observations.resize(num_observations);
     
-    cameras.resize(num_cameras_);
-    points3d.resize(num_points_);
+    cameras.resize(num_cameras);
+    points3d.resize(num_points);
 
-    for(int i = 0; i < num_observations_; ++i)
-    {
-      file >> camera_index_[i]
-           >> point_index_[i]
-           >> observations_[i].x()
-           >> observations_[i].y();
-    }
+    for(size_t i = 0; i < observations.size(); ++i)
+      file >> camera_index[i]
+           >> point_index[i]
+           >> observations[i].x()
+           >> observations[i].y();
 
-    for(int i = 0; i < num_cameras_; ++i)
+    for(size_t i = 0; i < num_cameras; ++i)
     {
       std::array<double,9> params;
-      for(int k = 0 ; k < 9 ; ++k)
+      for(size_t k = 0 ; k < 9 ; ++k)
         file >> params[k];
-      cameras[i].apply_rotation(params.data());
+      lma::apply_increment(cameras[i],params.data(),lma::Adl{});
     }
 
-    for(int i = 0; i < num_points_; ++i)
-    {
+    for(size_t i = 0; i < num_points; ++i)
       file >> points3d[i].x() 
            >> points3d[i].y()
            >> points3d[i].z();
-    }
 
     return true;
   }
 
-  int num_cameras_;
-  int num_points_;
-  int num_observations_;
-  int num_parameters_;
-
-  std::vector<int> point_index_;
-  std::vector<int> camera_index_;
+  std::vector<int> point_index;
+  std::vector<int> camera_index;
 
   template<class T> using AlignedVector = std::vector<T,Eigen::aligned_allocator<T>>;
   AlignedVector<Eigen::Vector3d> points3d;
-  AlignedVector<Eigen::Vector2d> observations_;
+  AlignedVector<Eigen::Vector2d> observations;
   AlignedVector<Camera> cameras;
 };
-
-
-void apply_small_rotation(Eigen::Matrix3d &m, double x, double y, double z)
-{
-  Eigen::Matrix3d skrew;
-  skrew << 1, -z, y, z, 1, -x, -y, x, 1;
-  m *= skrew;
-}
-
-namespace lma
-{
-	//namespace detail {
-
-
-		// Update policy of a Camera
-		void apply_increment(Camera& camera, const double delta[9], const Adl&)
-		{
-			camera.apply_rotation(delta);
-		}
-
-		// Only for numerical derivative:
-		// Update policy of a Camera according to the Ie parameter (h ~ 1e-8).
-		template<int I> void apply_small_increment(Camera& camera, double h, v::numeric_tag<I>, const Adl&)
-		{
-			if (I < 3) apply_small_rotation(camera.rotation, I == 0 ? h : 0, I == 1 ? h : 0, I == 2 ? h : 0);
-			else if (I == 3) camera.translation.x() += h;
-			else if (I == 4) camera.translation.y() += h;
-			else if (I == 5) camera.translation.z() += h;
-			else if (I == 6) camera.a += h;
-			else if (I == 7) camera.b += h;
-			else if (I == 8) camera.c += h;
-		}
-	//}
-
-  
-  //degree of freedom of a Camera
-  template<> struct Size<Camera> { enum {value = 9}; };
-
-  // Nothing to specify for the 3D points (Eigen::Vector3d) :
-  //   - degree of freedom of an Eigen::Vector is the size.
-  //   - The update policy of an Eigen::Vector is trivial.
-
-}
 
 
 struct Reprojection
 {
   const Eigen::Vector2d& obs;
-
   Reprojection(const Eigen::Vector2d& p2d):obs(p2d){}
 
-  bool operator()(const Camera& camera, const Eigen::Vector3d& point, Eigen::Vector2d& error) const
+  bool operator()(const Camera& camera, const Eigen::Vector3d& point, double (&error)[2]) const
   {
     const Eigen::Vector3d p = camera.rotation * point + camera.translation;
-    const double xp = - p[0] / p[2],
+    const double
+      xp = - p[0] / p[2],
       yp = - p[1] / p[2],
-      & l1 = camera.b,
-      & l2 = camera.c,
       r2 = xp*xp + yp*yp,
-      distortion = double(1.0) + r2  * (l1 + l2  * r2);
+      distortion = 1.0 + r2  * (camera.b + camera.c  * r2);
 
     error[0] = camera.a * distortion * xp - obs[0];
     error[1] = camera.a * distortion * yp - obs[1];
@@ -188,11 +170,11 @@ void call_lma(std::string file, double lambda, int iteration_max)
 
   lma::Solver<Reprojection> solver(lambda,iteration_max);
 
-  for (int i = 0; i < bal_problem.num_observations_; ++i)
+  for (size_t i = 0; i < bal_problem.observations.size(); ++i)
     solver.add(
-                Reprojection(bal_problem.observations_[i]),
-                &bal_problem.cameras[bal_problem.camera_index_[i]], 
-                &bal_problem.points3d[bal_problem.point_index_[i]]
+                Reprojection(bal_problem.observations[i]),
+                &bal_problem.cameras[bal_problem.camera_index[i]], 
+                &bal_problem.points3d[bal_problem.point_index[i]]
               );
 
 #ifdef USE_TOON
@@ -207,9 +189,9 @@ void call_lma(std::string file, double lambda, int iteration_max)
 int main(int argc, char** argv)
 {
   if (argc == 4) 
-    call_lma(argv[1],boost::lexical_cast<int>(argv[2]),boost::lexical_cast<int>(argv[3]));
+    call_lma(argv[1],boost::lexical_cast<double>(argv[2]),boost::lexical_cast<int>(argv[3]));
   else
-    std::cerr << "usage: test-lma3d <bal_problem> lambda nb_iteration_max\n";
+    std::cerr << "usage: test-lma3d <bal_problem> initial_lambda nb_iteration_max\n";
 
   return 0;
 }
